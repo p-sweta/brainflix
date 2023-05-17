c5dbf5ed42dd39787fe9e37a862365436933a544
Sweta <spmac@swetas-mbp.lan>
2023-05-17 00:23:47 -0400

    start jsx for header component, add fonts routes

--- src/components/Component/Header.js
+++ src/components/Component/Header.js
@@ -1,11 +1,29 @@
 import './Header.scss';
-import logo from '../../assets/logo/BrainFlix-logo.svg'
+import logo from '../../assets/logo/BrainFlix-logo.svg';
+import search from '../../assets/icons/search.svg';
+import avatar from '../../assets/images/Mohan-muruge.jpg';
+import upload from '../../assets/icons/upload.svg'
 
 const Header = () => {
     return (
-        <header className='header'>
-            <div className="header__logo-container">
-                <img className="header__logo" src={logo} alt="" />
+        <header className = 'header'>
+            <div className = "header__logo-container">
+                <img className = "header__logo" src = {logo} alt = "" />
+            </div>
+            <div>
+                <div>
+                    <img src = {search} alt = "" />
+                    <h3>Search</h3>
+                </div>
+                <img
+                className = "image"
+                src = {avatar}
+                alt="mohan-muruge-avatar"
+              />
+            </div>
+            <div>
+                <img src = {upload} alt="" />
+                <h3>UPLOAD</h3>
             </div>
         </header>
     )
