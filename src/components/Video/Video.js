0973e1f30503153ba747966ea3b006040ae39c39
Sweta <spmac@swetas-mbp.lan>
2023-05-18 20:06:42 -0400

    cleanup extra lines

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,11 +4,7 @@ import "./Video.scss";
 const Video = (props) => {
   return (
     <div className="hero__video-container">
-      <video
-        className="hero__video"
-        poster={props.video.image}
-        controls
-      >
+      <video className="hero__video" poster={props.video.image} controls>
         {/* <source src={props.video.video} /> */}
       </video>
     </div>
